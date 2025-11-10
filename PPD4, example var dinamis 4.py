5a8cbf0884fd032b55bb54479aebf985a2dde01d
Akram Junaidi <250306033.mhs@uinmataram.ac.id>
2025-11-10 16:59:13 +0700

    Delete PPD4, example var dinamis 4.py

--- PPD4, example var dinamis 4.py
+++
@@ -1,9 +0,0 @@
-#VARIABEL_DINAMIS_4
-UAS = int(input("masukkan nilai:"))
-if(UAS >= 90):
-    print("Nilai Anda Bagus!")
-elif(UAS >= 51 and UAS < 71):
-    print("Nilai Anda Kurang!")
-else:
-    print("Anda Tidak Lulus!")
-
